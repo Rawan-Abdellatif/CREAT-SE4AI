cd78ca112d31a7d2fbd7896a03596afeb331dff5
Rawan Abdellatif <121334455+Rawan-Abdellatif@users.noreply.github.com>
2023-06-27 23:22:09 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+// App.js
+import React from "react";
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
+import Homepage from "./component/Homepage";
+// import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      {/* <Homepage /> */}
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
